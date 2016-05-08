9ec5b143b65aae7466becaca7be9ffbc0e48e901
EvgeniOrmotsadze <eormo11@freeuni.edu.ge>
2016-05-08 14:24:58 +0400

    Hello Travis

---
+++ src/main/java/ge/evgo/sdp/HelloWorld.java
@@ -0,0 +1,13 @@
+package ge.evgo.sdp;
+
+/**
+ * Created by root_pc on 5/8/2016.
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Travis ! ");
+    }
+
+}
